6ddda8c43fb6cc92758d582ef7dd7a37558379b8
marzeq <marzeqmarzeq@gmail.com>
2022-11-16 00:10:56 +0100

    Tooltip animations

--- src/Components/Tooltip.tsx
+++ src/Components/Tooltip.tsx
@@ -1,12 +1,18 @@
 import { ReactNode, useState } from "react"
 import { Component } from "../util"
+import AppearAnimation from "./AppearAnimation"
 
 const Tooltip: Component<{
   text: string
   children: ReactNode
+  animate?: boolean
   setHovered?: (hovered: boolean) => void
-}> = ({ text, children, setHovered: externalSetHovered }) => {
+}> = ({ text, children, setHovered: externalSetHovered, animate = true }) => {
   const [hovered, setHovered] = useState(false)
+
+  const tTip = <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 min-w-max bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
+    {text}
+  </div>
 
   return (
     <div className="relative" onMouseEnter={() => {
@@ -17,9 +23,13 @@ const Tooltip: Component<{
       externalSetHovered?.(false)
     }}>
       {hovered && (
-        <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 min-w-max bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
-          {text}
-        </div>
+        animate ? (
+          <AppearAnimation time={0.1} delay={0}>
+            {tTip}
+          </AppearAnimation>
+        ) : (
+          tTip
+        )
       )}
       {children}
       
