f950bf88f53fd556c1d3476e6dd853949a914746
marzeq <marzeqmarzeq@gmail.com>
2022-11-16 00:18:02 +0100

    remove ttip animation

--- src/Components/Tooltip.tsx
+++ src/Components/Tooltip.tsx
@@ -1,18 +1,12 @@
 import { ReactNode, useState } from "react"
 import { Component } from "../util"
-import AppearAnimation from "./AppearAnimation"
 
 const Tooltip: Component<{
   text: string
   children: ReactNode
-  animate?: boolean
   setHovered?: (hovered: boolean) => void
-}> = ({ text, children, setHovered: externalSetHovered, animate = true }) => {
+}> = ({ text, children, setHovered: externalSetHovered}) => {
   const [hovered, setHovered] = useState(false)
-
-  const tTip = <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 min-w-max bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
-    {text}
-  </div>
 
   return (
     <div className="relative" onMouseEnter={() => {
@@ -22,15 +16,10 @@ const Tooltip: Component<{
       setHovered(false)
       externalSetHovered?.(false)
     }}>
-      {hovered && (
-        animate ? (
-          <AppearAnimation time={0.25} delay={0}>
-            {tTip}
-          </AppearAnimation>
-        ) : (
-          tTip
-        )
-      )}
+      {hovered &&
+        <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 min-w-max bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
+          {text}
+        </div>}
       {children}
       
     </div>
