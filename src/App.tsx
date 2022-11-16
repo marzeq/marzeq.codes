b9cfc5012ef61078595c361c105aecace32e97f8
marzeq <marzeqmarzeq@gmail.com>
2022-11-16 18:05:16 +0100

    not bothered to write a message just get me out of this hell

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react"
+import { ReactNode, useState } from "react"
 import { TypeAnimation } from "react-type-animation"
 import CopyClick from "./Components/CopyClick"
 import Link from "./Components/Link"
 import Tooltip from "./Components/Tooltip"
-import { Favicon, Title } from "./main"
 import { Component } from "./util"
 
 const DISCORD_TAG = "marzeq#0001"
@@ -12,8 +11,6 @@ const App = () => {
   const [copied, setCopied] = useState(false)
 
   return (<>
-    <Title>marzeq.codes</Title>
-    <Favicon src="/favicon.png" />
     <main className="bg-gray-100 text-gray-900 min-h-screen dark:bg-gray-900 dark:text-gray-100 font-mono">
       <div className="flex justify-center">
         <div className="mx-auto px-4 py-12 max-w-3xl flex flex-col items-center gap-3">
@@ -54,7 +51,7 @@ const App = () => {
             ]}
           />
 
-          <h2 className="text-3xl font-bold mt-14">Find me</h2>
+          <h2 className="text-3xl font-bold mt-14" id="findme">Find me</h2>
 
           <div className="flex justify-center mt-4 text-2xl font-medium gap-5">
             <Link href="https://github.com/marzeq">GitHub</Link>
@@ -62,7 +59,6 @@ const App = () => {
             <Tooltip setHovered={hovered => !hovered && copied && setCopied(false)} text={copied ? "Copied to clipboard!" : `Copy ${DISCORD_TAG} to clipboard`}>
               <CopyClick styleAs="link" setClicked={setCopied} text={DISCORD_TAG}>Discord</CopyClick>
             </Tooltip>
-            {/* <Tooltip text="marzeqmarzeq@gmail.com"><Link href="mailto:marzeqmarzeq@gmail.com">Email</Link></Tooltip> */}
           </div>
 
           <div className="flex flex-col justify-center mt-12 text-2xl font-medium gap-5 text-center">
@@ -70,9 +66,11 @@ const App = () => {
 
             <div className="flex flex-col gap-3 text-left">
               <Project name="👀" description="👀 In progress... Check my social media for possible updates." />
-              <Project name="marzeq.codes" oss ossUrl="https://github.com/marzeq/new-website" description="This website" link="/" />
-              <Project name="selfhost-discord-music-bot" oss description="A self-hostable Docker image of a Discord music bot" link="https://hub.docker.com/repository/docker/marzeq/selfhost-discord-music-bot" />
-              <Tooltip text="I cannot guarantee their code is good"><Project name="Older projects" description="See all my older projects on GitHub." link="https://github.com/marzeq?tab=repositories" /></Tooltip>
+              <Project name="marzeq.codes" oss="https://github.com/marzeq/new-website" description="This website" link="/" />
+              <Project name="selfhost-discord-music-bot" oss description="A self-hostable Docker image of a Discord music bot" link="https://hub.docker.com/repository/docker/marzeq/selfhost-discord-music-bot">
+                <BlueLink href="/#findme" notBlank>Contact me</BlueLink> if you want me to give you access to my instance.
+              </Project>
+              <Tooltip text="I cannot guarantee their code quality is up to my current standards"><Project name="Older projects" description="See all my older projects on GitHub." link="https://github.com/marzeq?tab=repositories" /></Tooltip>
             </div>
           </div>
         </div>
@@ -83,19 +81,30 @@ const App = () => {
 
 export default App
 
+
+const BlueLink: Component<{
+  href: string
+  notBlank?: boolean
+  children: ReactNode
+}> = ({ href, children, notBlank }) => {
+  return (<a href={href} target={!notBlank ? "_blank" : undefined} className="mt-2 text-sm text-blue-500 hover:text-blue-300 hover:underline transition duration-300">{children}</a>)
+}
+
+
 const Project: Component<{
   name: string
   description: string
   link?: string
-  oss?: boolean
-  ossUrl?: string
-}> = ({ name, description, link, oss, ossUrl }) => {
+  oss?: boolean | string
+  children?: ReactNode
+}> = ({ name, description, link, oss, children }) => {
   return (
     <a href={link} target="_blank">
-      <div className="max-w-xl mx-6 bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition duration-300 rounded p-4 cursor-pointer">
+      <div className="select-none max-w-xl border-x-2 border-gray-500 bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition duration-300 rounded p-4 cursor-pointer">
         <h3 className="text-xl font-bold">{name}</h3>
         <p className="mt-2 w-full text-base">{description}</p>
-        {oss ? (ossUrl ? <a href={ossUrl} target="_blank" className="mt-2 text-sm text-blue-500 hover:text-blue-300 hover:underline transition duration-300">Source code</a> : <span className="mt-2 text-sm">Source code available soon</span>) : null}
+        {oss ? (typeof oss === "string" ? <BlueLink href={oss}>Source code</BlueLink> : <span className="mt-2 text-sm">Source code available soon</span>) : null}
+        {children && <div className="mt-1 text-sm">{children}</div>}
       </div>
     </a>
   )
--- src/Components/Tooltip.tsx
+++ src/Components/Tooltip.tsx
@@ -17,8 +17,10 @@ const Tooltip: Component<{
       externalSetHovered?.(false)
     }}>
       {hovered &&
-        <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 min-w-max bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
-          {text}
+        <div className="absolute bottom-full left-1/2 -translate-y-2 transform -translate-x-1/2 w-screen flex justify-center m-0 p-0">
+          <div className="drop-shadow-lg bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded p-2 text-sm font-medium cursor-default select-none flex justify-center">
+            {text}
+          </div>
         </div>}
       {children}
       
